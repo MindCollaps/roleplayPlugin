dc78978bd2af1d789fe894340f3d168d6d7b83b4
Shadopaku <Shadopaku@gmail.com>
2019-05-08 16:33:49 +0200

    Pref

--- src/main/java/com/neocop/roleplayplugin/utils/Preferences.java
+++ src/main/java/com/neocop/roleplayplugin/utils/Preferences.java
@@ -26,39 +26,39 @@ public class Preferences {
 
     public static final String commandModDetectiveAnalyze = "analyze";
 
-    public static final String noPermissionOpNeeded = "§cDu hast keine berechtigung für diesen Command! Nur Op´s haben berechtigung!";
+    public static final String noPermissionOpNeeded = "§cDu hast nicht die benötigte Berechtigung um diesen Befehl auszuführen!";
     public static final String noPermissionCommandOnlyForUser = "Dieser Command ist nur für den User anwendbar!";
-    public static final String noPermissionAlreadyVoted = "§cDu hast bereits deine Stimme abgegeben!";
-    public static final String noPermissionDetectiveRoleNeeded = "§cDu musst detective sein, um diesen Befehl auszuführen!";
+    public static final String noPermissionAlreadyVoted = "§cDu hast bereits eine Wahl getroffen!";
+    public static final String noPermissionDetectiveRoleNeeded = "§cNur der Detectiv kann diesen Befehl ausführen!";
     public static final String noPermissionAlreadyDead = "§cDu bist bereits gestorben!";
-    public static final String noPermissionVoteNotActive = "§cVoten ist nur während der Vote Phase möglich!";
-    public static final String notEnoughPlayerForRpg = "§eEs sind nicht genug Spieler in der Spieler Liste. Füge sie mit dem add command hinzu!";
-    public static final String noRunningRpg = "§cEs ist kein Roleplay im gange!";
+    public static final String noPermissionVoteNotActive = "§cDu kannst jetzt noch nicht Voten. Warte bis die Votephase anfängt.";
+    public static final String notEnoughPlayerForRpg = "§eEs sind nicht genug Spieler in der Spielerliste. Du kannst neue Spieler mit dem Befehl: /rpg add [name] hinzufügen.";
+    public static final String noRunningRpg = "§cMomentan findet leider kein Roleplay statt";
 
-    public static final String playerAddToRpg = "§aDu wurdest zum Roleplay hinzugefügt!";
-    public static final String playerNotFound = "§cSpieler konnte nicht gefunden werden oder ist nicht Online!";
-    public static final String playerRpgListEmpty = "§eDie Spieler Liste ist leer, füge neue Spieler mit dem add befehl hinzu!";
-    public static final String playerRemoveFromRpg = "§eDu wurdest von Roleplay entfernt!";
-    public static final String playerAlreadyInRpg = "§eDer Spieler ist bereits im Roleplay!";
-    public static final String playerIsNotInRpg = "§eDer Spieler ist nicht im Roleplay!";
+    public static final String playerAddToRpg = "§aDu bist dem Roleplay beigetreten.";
+    public static final String playerNotFound = "§cDieser Spieler wurde nicht gefunden.";
+    public static final String playerRpgListEmpty = "§eMomentan nimmt niemand am Roleplay teil. Du kannst neue Spieler mit dem Befehl: /rpg add [name] hinzufügen";
+    public static final String playerRemoveFromRpg = "§eDu whast das Roleplay verlassen.";
+    public static final String playerAlreadyInRpg = "§eDieser Spieler nimmt schon am Roleplay teil.";
+    public static final String playerIsNotInRpg = "§eDieser Spieler nimmt nicht am Roleplay teil.";
     public static final String playerAddToVoteList = "§aSpieler wurde auf die Votelist gesetzt!";
     public static final String playerDiedByVoteKill = "§eDu wurdest so eben von der Abstimmung getötet, verlasse gegebenfalls bitte den Sprachchat, falls eure Roleplay Gruppe einen verwendet!";
     public static final String playerDiedByOtherPlayer = "§eDu wurdest so eben von einem Spieler Getötet, verlasse gegebenfalls bitte den Sprachchat, falls eure Roleplay Gruppe einen verwendet!";
 
-    public static final String rpgKillerNightAction = "Guten Abend\n§cWähle, wen du töten willst!";
-    public static final String rpgKillerDayAction = "Guten Morgen!\nVersuche deine Taten zu verschleiern ;3";
+    public static final String rpgKillerNightAction = "Der Mond leuchtet heute Abend sehr hell.\nWenn möchtest du umbringen?§c";
+    public static final String rpgKillerDayAction = "Hat es Spaß gemacht?\nDu solltest besser nicht endeckt werden. Es könnte für dich übel ausgehen.";
 
-    public static final String rpgVillagerDayAction = "Guten Morgen!\nVersuche durchs Gespräch heraus zu finden wer jemanden getötet haben könnte!";
-    public static final String rpgVillagerNightAction = "Nachti :3\nFür dich gibt es Nachts nichts zu tun! Gute nacht :3";
+    public static final String rpgVillagerDayAction = "Wach auf!\nEin SPieler wurde umgebracht! Du musst den Mörder überführen!";
+    public static final String rpgVillagerNightAction = "Ein einfacher Bürger sollte abends nicht alleine wach sein.\nDu solltest besser Schlafen gehen.";
 
-    public static final String rpgDetectivNightAction = "Guten Abend\nFür dich gibt es Nachts nichts zu tun! Gute nacht :3";
-    public static final String rpgDetectivDayAction = "Guten Abend\nVersuche durchs Gespräch heraus zu finden wer jemanden getötet haben könnte! Falls du willst, kannst du deinen Lügendetektor benutzen, dies machst du mit dem /detectiv befehl!";
+    public static final String rpgDetectivNightAction = "Die Bürger verlassen sich auf dich!\nDu solltest daher deine Kräfte schonen.";
+    public static final String rpgDetectivDayAction = "Wach auf, ein Spieler wurde umgebracht!\nDu kannst deinen Lügendetektor benutzen, um den Übeltäter zu identifizieren. Um diesen zu benutzen, führe einfach den Befehl : /detectiv analyze [name] aus.";
 
-    public static final String succPlayerAddToRpg = "§aSpieler wurde dem Roleplay hinzugefügt!";
-    public static final String succPlayerRemoveFromRpg = "§aSpieler wurde vom Roleplay entfernt";
+    public static final String succPlayerAddToRpg = "§aDieser Spieler wurde dem Roleplay hinzugefügt!";
+    public static final String succPlayerRemoveFromRpg = "§aDieser Spieler wurde aus dem Roleplay entfernt";
 
-    public static final String globalPlayerDiedByOtherPlayer = "§eEin Spieler wurde soeben von einer unbekannten Person getötet. Findet den Mörder!";
-    public static final String globalRpgGetCanceled = "§cRoleplay wurde unterbrochen!";
+    public static final String globalPlayerDiedByOtherPlayer = "§eEin Spieler ist gestorben! Findet den Mörder!";
+    public static final String globalRpgGetCanceled = "§cDas Roleplay wurde unterbrochen!";
     public static final String globalRules = striche + "§e§lRules§r\n\nEs geht sofort los, nur noch ein paar grundlegende Sachen. \nEigentlich ganz einfach:\n-Vorzeitiges Sterben (durch äußere einflüsse) oder verlassen des Servers, führt zum Spielauschluss\n-Reportet Bugs\n-Seid nett\n-Habt spaß" + striche;
     public static final String globalNobodyDied = "§eDas dorf hat sich nicht entscheiden können, wer stirbt!";
     public static final String globalVoteBegin = striche + "§2§lVoting§r\n\nBitte stimmt jetzt mit dem vote command ab, wer diese Runde sterben soll. Es müssen mindestens 3/4 dafür sein, sonst findet keine Hinrichtung statt!" + striche;
@@ -81,7 +81,7 @@ public class Preferences {
 
     public static final String helpDetectivCommon = "/" + commandNameDetective + " " + commandModDetectiveAnalyze + " - Zeigt alle Spieler an, die einem Lügendetektortest unterzogen werden können.";
 
-    public static final String killer = striche + "\n§c§lKiller\n\n§r§dDu hast jede Nacht die Möglichkeit eine Person zu Töten! Dies tust du über das nächtlich erscheinende Inventar!\n" + striche;
-    public static final String villager = striche + "\n§a§lVillager\n\n§r§dDu bist ein ganz normaler Dorfbewohner, doch nicht unnötig, sondern ein wichtiger Bestandteil des gesammten Dorfteams. Jeden Tag kannst du abstimmen, wer getötet werden soll! Nutze deine Stimme sinnvoll ;3\n" + striche;
-    public static final String detectiv = striche + "\n§9§lDetektiv\n\n§r§dDu bist eine sehr wichtige Rolle, nämlich der Detektiv. Jeden Tag hast du das Sagen und kannst pro Spiel zwie Person mit einer Erfolgschance von 40% einen Lügentest machen lassen, schlägt er aus, weißt du, dass etwas nicht in ordung ist, gibt er allerdings keinen muchs von sich, war es wohl ein Fehlschlag, oder er ist ein Dorfbewohner. Pass auf die Bürger auf ;3\n" + striche;
+    public static final String killer = striche + "\n§c§lMörder\n\n§r§dDu solltest jede Nacht einen Spieler umbringen! Ich, an deiner Stelle würde jeden umbringen. Es wird sicher viel Süaß machen, vertrau mir.\n" + striche;
+    public static final String villager = striche + "\n§a§lEinwohner\n\n§r§dDu bist ein ganz normaler Einwohner dieser Stadt. Aber dennoch nicht unwichtig! Du hast eine STimme die du dafür benutzen kannst den Übeltäter zu überführen. Handel Weise!\n" + striche;
+    public static final String detectiv = striche + "\n§9§lDetektiv\n\n§r§dDu bist sozusagen der Sheriff dieser Stadt. Niemand entkommt deiner Gerechtigkeit! Zumindest ist das so geplant. Also benutze deine Fähigkeiten Weise um deine Mitspieler zu beschützen! Du kannst mit deinem Lügendetektor herausfinden, ob ein Mitspieler korrumpiert ist. Doch manchmal kann eine Lüge selbst den lügendetektor austricksen. Also, sei achtsam!\n" + striche;
 }
